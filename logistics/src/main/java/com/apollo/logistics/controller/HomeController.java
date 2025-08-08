e6dbb04718dee12718261fb02c8b85f6017a08ac
yrel13 <obdamen.johnyrel@gmail.com>
2025-08-08 19:43:30 +0800

    Organized controller java files; Mistake: tested jar properties

--- logistics/src/main/java/com/apollo/logistics/controller/HomeController.java
+++ logistics/src/main/java/com/apollo/logistics/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.apollo.logistics;
+package com.apollo.logistics.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
