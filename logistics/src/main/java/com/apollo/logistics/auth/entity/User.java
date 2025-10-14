bcd37e4d81257635511cc558820cc3cc85be02dc
yrel13 <obdamen.johnyrel@gmail.com>
2025-10-14 22:28:31 +0800

    DEV: User.java = WIP; Next: pgsql db users table;

--- logistics/src/main/java/com/apollo/logistics/auth/entity/User.java
+++ logistics/src/main/java/com/apollo/logistics/auth/entity/User.java
@@ -1,12 +1,20 @@
 package com.apollo.logistics.auth.entity;
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.userdetails.UserDetails;
 import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
 
 @Entity
 @Table(name="users")
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class User {
-    @Id @GeneratedValue(strategy=GenerationType.IDENTITY) private Long id;
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY) private Long id;
     @Column(unique=true,nullable=false) private String username;
     @Column(unique=true,nullable=false) private String email;
     @Column(nullable=false) private String password; // bcrypt
