8630142235e86622a67d86fda0b48a4a44424988
yrel13 <obdamen.johnyrel@gmail.com>
2025-10-26 23:28:13 +0800

    DEV: User.java; WIP: debug User.java;

--- logistics/src/main/java/com/apollo/logistics/auth/entity/User.java
+++ logistics/src/main/java/com/apollo/logistics/auth/entity/User.java
@@ -69,11 +69,74 @@ public class User implements UserDetails {
         return id;
     }
 
-    public Long setId(Long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdat;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedat;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(
+            new SimpleGrantedAuthority("ROLE_" + role.toUpperCase())
+        );
+    }
 }
 // public class User{
 //     @Id
