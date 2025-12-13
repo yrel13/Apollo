9290e1085445879d09f7ea8ed1605cf510a3c393
yrel13 <obdamen.johnyrel@gmail.com>
2025-12-13 20:22:19 +0800

    DEV: OpenApiConfig.java: OpenAPI/Swagger for API endpoint documentation;

---
+++ logistics/src/main/java/com/apollo/logistics/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.apollo.logistics.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Apollo Logistics API")
+                        .version("1.0.0")
+                        .description("API documentation for Apollo Logistics System"));
+    }
+}
